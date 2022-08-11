42660c2c796ce6ef9e2f58b7522e0b9218cf2ab0
Aldi Wijaya <aldiwijaya.mia8@gmail.com>
2022-08-11 20:46:17 +0700

    Replace entire system
    Replace old code with new code and new board (Lynx32)

--- antares.id-lora/antares.id-lora.ino
+++ antares.id-lora/antares.id-lora.ino
@@ -1,57 +1,121 @@
-// Create by antares.id
-// edited by Aldi Wijaya
-// For more information, please visit https://antares.id/id/docs.html
+#include <lorawan.h>
 
-#include <loraid.h>
-#define ACCESSKEY "antares.id_accesskey"
-#define DEVICEID "antares.id_loradeviceaddress"
+//ABP Credentials
+const char *devAddr = "antares.id_loradeviceaddress";
+// antares.id_accesskey xxxxxxxxxxxxxxxx:xxxxxxxxxxxxxxxx
+const char *nwkSKey = "xxxxxxxxxxxxxxxx0000000000000000";
+const char *appSKey = "0000000000000000xxxxxxxxxxxxxxxx";
 
-long interval = 60000;    // 60 s interval to send message
-long previousMillis = 0;  // will store last time message sent
+const unsigned long interval = 600000;    // 30 min interval to send message
+unsigned long previousMillis = 0;  // will store last time message sent
 unsigned int counter = 0;     // message counter
 
-void setup() {
-  // Set Serial Monitor Baudrate
-  Serial.begin(9600);
-  
-  // Setup loraid access
-  lora.init();
+char myStr[50];
+byte outStr[255];
+byte recvStatus = 0;
+int port, channel, freq;
+bool newmessage = false;
 
-  // Set LoRaWAN Class
+const sRFM_pins RFM_pins = {
+  .CS = 5,
+  .RST = 0,
+  .DIO0 = 27,
+  .DIO1 = 2,
+};
+
+void setup() {
+  // Setup loraid access
+  Serial.begin(115200);
+  delay(2000);
+  if (!lora.init()) {
+    Serial.println("RFM95 not detected");
+    delay(5000);
+    return;
+  }
+
+  // Set LoRaWAN Class change CLASS_A or CLASS_C
   lora.setDeviceClass(CLASS_A);
 
   // Set Data Rate
-  lora.setDataRate(2);
-  
-  // Put Antares Key and DevAddress here
-  lora.setAccessKey(ACCESSKEY);
-  lora.setDeviceId(DEVICEID);
+  lora.setDataRate(SF10BW125);
+
+  // Set FramePort Tx
+  lora.setFramePortTx(5);
+
+  // set channel to random
+  lora.setChannel(MULTI);
+
+  // Set TxPower to 15 dBi (max)
+  lora.setTxPower(15);
+
+  // Put ABP Key and DevAddress here
+  lora.setNwkSKey(nwkSKey);
+  lora.setAppSKey(appSKey);
+  lora.setDevAddr(devAddr);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  char myStr[50];
-  char outStr[255];
-  int recvStatus = 0;
-  
-  unsigned long currentMillis = millis();
-
   // Check interval overflow
-  if(currentMillis - previousMillis > interval) {
-    previousMillis = currentMillis; 
+  if (millis() - previousMillis > interval) {
+    previousMillis = millis();
 
-    sprintf(myStr, "Test LoRaWAN Telkom %d ", counter); 
+    sprintf(myStr, "Lora Counter-%d", counter++);
 
-    lora.sendToAntares((unsigned char *)myStr, strlen(myStr), 0);
-    counter++;
+    
+    Serial.print("Sending: ");
     Serial.println(myStr);
+    lora.sendUplink(myStr, strlen(myStr), 0);
+    port = lora.getFramePortTx();
+    channel = lora.getChannel();
+    freq = lora.getChannelFreq(channel);
+    Serial.print(F("fport: "));    Serial.print(port);Serial.print(" ");
+    Serial.print(F("Ch: "));    Serial.print(channel);Serial.print(" ");
+    Serial.print(F("Freq: "));    Serial.print(freq);Serial.println(" ");
   }
 
-  recvStatus = lora.readData(outStr);
-  if(recvStatus) {
-    Serial.println(outStr);
-  }
-  
   // Check Lora RX
   lora.update();
+
+  recvStatus = lora.readDataByte(outStr);
+  if (recvStatus) {
+    newmessage = true;
+    int counter = 0;
+    port = lora.getFramePortRx();
+    channel = lora.getChannelRx();
+    freq = lora.getChannelRxFreq(channel);
+
+    for (int i = 0; i < recvStatus; i++) {
+      if (((outStr[i] >= 32) && (outStr[i] <= 126)) || (outStr[i] == 10) || (outStr[i] == 13))
+        counter++;
+    }
+    if (port != 0) {
+      if (counter == recvStatus) {
+        Serial.print(F("Received String : "));
+        for (int i = 0; i < recvStatus; i++) {
+          Serial.print(char(outStr[i]));
+        }
+      }
+      else {
+        Serial.print(F("Received Hex: "));
+        for (int i = 0; i < recvStatus; i++)
+        {
+          Serial.print(outStr[i], HEX); Serial.print(" ");
+        }
+      }
+      Serial.println();
+      Serial.print(F("fport: "));    Serial.print(port);Serial.print(" ");
+      Serial.print(F("Ch: "));    Serial.print(channel);Serial.print(" ");
+      Serial.print(F("Freq: "));    Serial.println(freq);Serial.println(" ");
+    }
+    else {
+      Serial.print(F("Received Mac Cmd : "));
+      for (int i = 0; i < recvStatus; i++) {
+        Serial.print(outStr[i], HEX); Serial.print(" ");
+      }
+      Serial.println();
+      Serial.print(F("fport: "));    Serial.print(port);Serial.print(" ");
+      Serial.print(F("Ch: "));    Serial.print(channel);Serial.print(" ");
+      Serial.print(F("Freq: "));    Serial.println(freq);Serial.println(" ");
+    }
+  }
 }
